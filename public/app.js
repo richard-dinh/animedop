c11ed11a617dee953e3f6b2d7a51db39e99ef3cf
richard-dinh <richarddinh@gmail.com>
2020-04-27 16:34:43 -0700

    wrote test axios call

--- public/app.js
+++ public/app.js
@@ -1,11 +1,18 @@
 axios.get('/api/anime')
-.then(data => {
+.then(({data}) => {
+  document.getElementById('name').textContent = data[0].name
+  document.getElementById('mal').textContent = data[0].mal
   let results = document.getElementById('results')
-  for(let i = 0; i< data.length; i++){
+  console.log(data)
+  for(let i = 1; i< data.length; i++){
     let listItem = document.createElement('li')
     listItem.innerHTML = `
-    name: 
+    number: ${data[i].number}<br>
+    title: ${data[i].title}<br>
+    link: ${data[i].link}<br>
+    episode(s): ${data[i].episode || 'N/A'}
     `
+    results.append(listItem)
   }
 })
 .catch(err => console.err(err))
