6a07521ed2225b718df9ed0ff672709e75938b5f
richard-dinh <richarddinh@gmail.com>
2020-04-28 14:26:40 -0700

    created renderResults function

--- public/app.js
+++ public/app.js
@@ -20,25 +20,22 @@ let anime
 // })
 // .catch(err => console.err(err))
 
-document.getElementById('submit').addEventListener('click', event => {
-  //prevent default of a button in a a form
-  event.preventDefault()
-  //run api call to get titles
-  axios.get(`https://api.jikan.moe/v3/search/anime?q=${document.getElementById('search').value}&limit=30`)
-  .then( ({data : {results}}) => {
-    //jikan returns the anime titles in the results key
-    console.log(results)
-    for(let i = 0; i < results.length; i++){
-      //need 2 titles for the way animeThemes sets its starting year (can specify a year: 1999 or say 90s)
-      //only create cards for anime that have already aired
-      if(results[i].start_date !== null){
-        let animeNode = document.createElement('div')
-        //get title
-        animeNode.dataset.title = results[i].title
-        //get the start year
-        animeNode.dataset.year = results[i].start_date.substring(0, 4)
-        animeNode.setAttribute('class', 'card col-sm-6 col-md-4')
-        animeNode.innerHTML = `
+const renderResults = title => {
+  axios.get(`https://api.jikan.moe/v3/search/anime?q=${title}&limit=30`)
+    .then(({ data: { results } }) => {
+      //jikan returns the anime titles in the results key
+      console.log(results)
+      for (let i = 0; i < results.length; i++) {
+        //need 2 titles for the way animeThemes sets its starting year (can specify a year: 1999 or say 90s)
+        //only create cards for anime that have already aired
+        if (results[i].start_date !== null) {
+          let animeNode = document.createElement('div')
+          //get title
+          animeNode.dataset.title = results[i].title
+          //get the start year
+          animeNode.dataset.year = results[i].start_date.substring(0, 4)
+          animeNode.setAttribute('class', 'card col-sm-6 col-md-4 cardStyle')
+          animeNode.innerHTML = `
           <img src="${results[i].image_url}" class="card-img-top" alt="${results[i].title} placeholder">
           <div class="card-body">
             <h5 class="card-title">
@@ -50,11 +47,20 @@ document.getElementById('submit').addEventListener('click', event => {
             <a class="btn btn-primary searchResult">Watch Op/Ed</a>
           </div>
           `
-        document.getElementById('results').append(animeNode)
+          document.getElementById('results').append(animeNode)
+        }
       }
+    })
+    .catch(err => console.error(err))
+}
+
+
+//event listener on all searchResults
+document.addEventListener('click', event => {
+  if(event.target.id === 'submit') {
+    event.preventDefault()
+    renderResults(document.getElementById('search').value)
     //empty out user input
     document.getElementById('search').value = ''
   }
-  })
-  .catch(err => console.error(err))
 })
