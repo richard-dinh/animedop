8db8dee0b8113056716873a9c48664c0960ba6c2
richard-dinh <richarddinh@gmail.com>
2020-04-28 13:26:02 -0700

    set search button to get titles from jikan api

--- public/app.js
+++ public/app.js
@@ -16,3 +16,18 @@ axios.get('/api/anime')
   }
 })
 .catch(err => console.err(err))
+
+document.getElementById('submit').addEventListener('click', event => {
+  //prevent default of a button in a a form
+  event.preventDefault()
+  let animeTitles = []
+  //run api call to get titles
+  axios.get(`https://api.jikan.moe/v3/search/anime?q=${document.getElementById('search').value}&limit=30`)
+  .then( ({data: titles}) => {
+    //jikan returns the anime titles in the results key
+    console.log(titles.results)
+    //empty out user input
+    document.getElementById('search').value = ''
+  })
+  .catch(err => console.error(err))
+})
