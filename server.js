148c66ed7cbadea63b128bd6b46a078c9c6ddbcc
richard-dinh <richarddinh@gmail.com>
2020-05-06 11:50:07 -0700

    set up server.js to support react

--- server.js
+++ server.js
@@ -5,9 +5,11 @@ const app = express()
 
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
-app.use(express.static(join(__dirname, 'public')))
-
+// app.use(express.static(join(__dirname, 'public')))
+app.use(express.static(join(__dirname, 'client', 'build')))
 //bring in routes
 app.use(require('./routes'))
-
+app.get('/*', (request, response) => {
+  response.sendFile(join(__dirname, 'client', 'build', 'index.html'))
+})
 app.listen(process.env.PORT || 3001)
