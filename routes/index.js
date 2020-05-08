ddae07e6800bd8bf6c59005f96091158ddb75f41
richard-dinh <richarddinh@gmail.com>
2020-05-08 13:55:46 -0700

    added youtube search to backend

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 const router = require('express').Router()
 
 router.use('/api', require('./animeRoutes.js'))
+router.use('/api', require('./youtubeRoutes.js'))
 
 module.exports = router
--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+require('dotenv').config()
 const express = require('express')
 const {join} = require('path')
 require('dotenv').config()
