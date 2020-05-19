f46e8392a930e5e1b4e2e1d06a9f51575f785494
David <davidedu322@gmail.com>
2020-05-18 17:38:37 -0700

    Add back the Searchbar component

---
+++ client/src/components/Searchbar.js
@@ -0,0 +1,91 @@
+import React, { useState, useContext, useEffect } from 'react'
+import { makeStyles } from '@material-ui/core/styles'
+import Paper from '@material-ui/core/Paper'
+import InputBase from '@material-ui/core/InputBase'
+import IconButton from '@material-ui/core/IconButton'
+import SearchIcon from '@material-ui/icons/Search'
+import { useHistory } from 'react-router-dom'
+import AnimeContext from '../utils/context/AnimeContext.js'
+const useStyles = makeStyles((theme) => ({
+  root: {
+    padding: '2px 4px',
+    display: 'flex',
+    alignItems: 'center',
+    width: 400,
+  },
+  input: {
+    marginLeft: theme.spacing(1),
+    flex: 1,
+  },
+  iconButton: {
+    padding: 10,
+  },
+  divider: {
+    height: 28,
+    margin: 4,
+  },
+}))
+
+const SearchBar = () => {
+  const classes = useStyles()
+  //use history for redirecting
+  let history = useHistory()
+
+  //state to hold search string
+  const [userInput, setUserInput] = useState('')
+
+  //bring in animeContext to store title
+  const {
+    search,
+    updateSearch,
+    resetState,
+    mal_id,
+    title,
+    newSearch,
+  } = useContext(AnimeContext)
+  const handleInputChange = (event) => {
+    setUserInput(event.target.value)
+  }
+
+  const handleSubmit = (event) => {
+    event.preventDefault()
+    // if(mal_id && title){
+    //   newSearch(userInput)
+    // }
+    // else
+    updateSearch('kaguya')
+    updateSearch(userInput)
+  }
+
+  //runs when search is updated
+  useEffect(() => {
+    if (search) {
+      //empty our search
+      //pushes user to route
+      console.log('ping')
+      setUserInput('')
+      history.push(`/search/${search}`)
+    }
+  }, [search])
+
+  return (
+    <Paper component='form' className={classes.root} onSubmit={handleSubmit}>
+      <InputBase
+        className={classes.input}
+        name='search'
+        onChange={handleInputChange}
+        placeholder='Search Anime'
+        value={userInput}
+      />
+      <IconButton
+        type='submit'
+        className={classes.iconButton}
+        aria-label='search'
+      >
+        <SearchIcon />
+      </IconButton>
+    </Paper>
+  )
+}
+
+export default SearchBar
