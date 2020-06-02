0c9e5340ee0ca020db836b5c1ff1ef1425d5995d
richard-dinh <richarddinh@gmail.com>
2020-06-02 15:52:14 -0700

    moved searchbar functionality over to appbar

--- client/src/components/Heading.js
+++ client/src/components/Heading.js
@@ -1,34 +1,22 @@
-import React from 'react'
+import React, { useState, useContext, useEffect } from 'react'
 import { makeStyles, fade } from '@material-ui/core/styles'
 import AppBar from '@material-ui/core/AppBar'
 import PolicyIcon from '@material-ui/icons/Policy'
 import Toolbar from '@material-ui/core/Toolbar'
 import Typography from '@material-ui/core/Typography'
-import Grid from '@material-ui/core/Grid'
-import Container from '@material-ui/core/Container'
 import SearchIcon from '@material-ui/icons/Search'
 import InputBase from '@material-ui/core/InputBase';
-import IconButton from '@material-ui/core/IconButton';
-import MenuIcon from '@material-ui/icons/Menu';
-import Search from './Searchbar.js'
-
+import { useHistory } from 'react-router-dom'
+import AnimeContext from '../utils/context/AnimeContext.js'
 const useStyles = makeStyles((theme) => ({
   icon: {
     marginRight: theme.spacing(2),
   },
-    iconFill: {
-      fill: "gray"
-    },
-  heroContent: {
-    //backgroundColor: theme.palette.background.paper,
-    //backgroundColor: 'rgba(255, 255, 255, 0.2)',
-    padding: theme.spacing(8, 0, 6),
-  },
-  heroButtons: {
-    marginTop: theme.spacing(4),
+  iconFill: {
+    fill: "gray"
   },
   background: {
-    backgroundColor: '#4caf50',
+    backgroundColor: '#5E784D',
   },
   search: {
     position: 'relative',
@@ -83,7 +71,44 @@ const useStyles = makeStyles((theme) => ({
 }))
 
 const Heading = () => {
+
   const classes = useStyles()
+
+  //use history for redirecting
+  let history = useHistory()
+
+  //state to hold search string
+  const [userInput, setUserInput] = useState('')
+
+  const {
+    search,
+    updateSearch,
+    resetState,
+    mal_id,
+    title,
+    newSearch,
+  } = useContext(AnimeContext)
+
+  const handleInputChange = (event) => {
+    setUserInput(event.target.value)
+  }
+
+  const handleSubmit = (event) => {
+    event.preventDefault()
+    updateSearch(userInput)
+  }
+
+  //runs when search is updated
+  useEffect(() => {
+    if (search) {
+      //empty our search
+      //pushes user to route
+      console.log('ping')
+      setUserInput('')
+      history.push(`/search/${search}`)
+    }
+  }, [search])
+
   return (
     <>
       <AppBar position='static' className={classes.background}>
@@ -92,22 +117,24 @@ const Heading = () => {
           <Typography className = {classes.title} variant='h6' color='inherit' noWrap>
             Animedop
           </Typography>
-          <div className={classes.search}>
+          <form className={classes.search} onSubmit={handleSubmit}>
             <div className={classes.searchIcon}>
               <SearchIcon className={classes.iconFill}/>
             </div>
             <InputBase
-              placeholder="Search…"
+              placeholder="Search..."
               classes={{
                 root: classes.inputRoot,
                 input: classes.inputInput,
               }}
-              inputProps={{ 'aria-label': 'search' }}
+              // inputProps={{ 'aria-label': 'search' }}
+              onChange={handleInputChange}
+              value={userInput}
             />
-          </div>
+          </form>
         </Toolbar>
       </AppBar>
-      <div className={classes.heroContent}>
+      {/* <div className={classes.heroContent}>
         <Container maxWidth='sm'>
           <Typography
             component='h1'
@@ -128,12 +155,12 @@ const Heading = () => {
           </Typography>
           <div className={classes.heroButtons}>
             <Grid container spacing={2} justify='center'>
-              {/* Search Bar here */}
+              Search Bar here
               <Search />
             </Grid>
           </div>
         </Container>
-      </div>
+      </div> */}
     </>
   )
 }
