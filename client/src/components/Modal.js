53c2260c0862f6f85778a08a84502f5c08dae1f7
richard-dinh <richarddinh@gmail.com>
2020-06-04 21:30:19 -0700

    added welcomeModal check in localStorage

---
+++ client/src/components/Modal.js
@@ -0,0 +1,97 @@
+import React, {useContext} from 'react';
+import PropTypes from 'prop-types';
+import { makeStyles } from '@material-ui/core/styles';
+import Modal from '@material-ui/core/Modal';
+import Backdrop from '@material-ui/core/Backdrop';
+import { useSpring, animated } from 'react-spring/web.cjs'; // web.cjs is required for IE 11 support
+import AnimeContext from '../utils/context/AnimeContext.js'
+const useStyles = makeStyles((theme) => ({
+  modal: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  paper: {
+    backgroundColor: theme.palette.background.paper,
+    border: '4px solid #5E784D',
+    // border: '4px solid #5D1D1D',
+    boxShadow: theme.shadows[5],
+    padding: theme.spacing(2, 4, 3),
+  },
+}));
+
+const Fade = React.forwardRef(function Fade(props, ref) {
+  const { in: open, children, onEnter, onExited, ...other } = props;
+  const style = useSpring({
+    from: { opacity: 0 },
+    to: { opacity: open ? 1 : 0 },
+    onStart: () => {
+      if (open && onEnter) {
+        onEnter();
+      }
+    },
+    onRest: () => {
+      if (!open && onExited) {
+        onExited();
+      }
+    },
+  });
+
+  return (
+    <animated.div ref={ref} style={style} {...other}>
+      {children}
+    </animated.div>
+  );
+});
+
+Fade.propTypes = {
+  children: PropTypes.element,
+  in: PropTypes.bool.isRequired,
+  onEnter: PropTypes.func,
+  onExited: PropTypes.func,
+};
+
+const SpringModal = () => {
+  const classes = useStyles();
+  const [open, setOpen] = React.useState(true);
+
+  //bring updateDisplayModal from AnimeContext
+  const { updateDisplayModal } = useContext(AnimeContext)
+  const handleOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+    localStorage.setItem('welcomeModal', true)
+  };
+
+  return (
+    <div>
+      {/* <button type="button" onClick={handleOpen}>
+        react-spring
+      </button> */}
+      <Modal
+        aria-labelledby="spring-modal-title"
+        aria-describedby="spring-modal-description"
+        className={classes.modal}
+        open={open}
+        onClose={handleClose}
+        closeAfterTransition
+        BackdropComponent={Backdrop}
+        BackdropProps={{
+          timeout: 500,
+        }}
+      >
+        <Fade in={open}>
+          <div className={classes.paper}>
+            <h2 id="spring-modal-title">Welcome to Animedop!</h2>
+            <p id="spring-modal-description">Search up Animes by title and watch their Opening and Endings!</p>
+          </div>
+        </Fade>
+      </Modal>
+    </div>
+  );
+}
+
+export default SpringModal
--- client/src/views/Home.js
+++ client/src/views/Home.js
@@ -1,7 +1,7 @@
 import React, {useEffect, useContext} from 'react';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import AnimeContext from '../utils/context/AnimeContext.js'
-
+import Modal from '../components/Modal.js'
 const Home = () => {
 
   const {updateSearch} = useContext(AnimeContext)
@@ -14,6 +14,8 @@ const Home = () => {
   return (
     <>
       <CssBaseline />
+      {/* Only display Modal once (when user first enters website). Once user closes modal, displayModal is set to false */}
+      {localStorage.getItem('welcomeModal') ? null : <Modal />}
     </>
   )
 }
