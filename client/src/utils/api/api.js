fbc3f73a1d5f063dca48d0813d75cb60f8971d1e
richard-dinh <richarddinh@gmail.com>
2020-06-10 14:59:20 -0700

    updated jikan search to accomadate for updated api

--- client/src/utils/api/api.js
+++ client/src/utils/api/api.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 
 const API = {
-  jikan: title => axios.get(`https://api.jikan.moe/v3/search/anime?q=${encodeURIComponent(title)}&limit=30`),
+  jikan: title => axios.get(`https://api.jikan.moe/v3/search/anime?q=${encodeURIComponent(title)}&order_by=members&sort=desc&limit=30`),
   getWiki: (mal_id, title) => axios.get(`/api/animesearch/${mal_id}/${encodeURIComponent(title)}`),
   getVideos: (title, wikiPage) => axios.get(`/api/anime/${encodeURIComponent(title)}/${encodeURIComponent(wikiPage)}`)
 }
