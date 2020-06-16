621520e0673197c98c4ce46ca6216bcf4cf86604
richard-dinh <richarddinh@gmail.com>
2020-06-16 13:47:01 -0700

    condense material ui imports in Modal.js

--- client/src/components/Modal.js
+++ client/src/components/Modal.js
@@ -1,8 +1,7 @@
 import React, {useContext} from 'react';
 import PropTypes from 'prop-types';
 import { makeStyles } from '@material-ui/core/styles';
-import Modal from '@material-ui/core/Modal';
-import Backdrop from '@material-ui/core/Backdrop';
+import {Modal, Backdrop} from '@material-ui/core'
 import { useSpring, animated } from 'react-spring/web.cjs'; // web.cjs is required for IE 11 support
 import AnimeContext from '../utils/context/AnimeContext.js'
 const useStyles = makeStyles((theme) => ({
@@ -68,9 +67,6 @@ const SpringModal = () => {
 
   return (
     <div>
-      {/* <button type="button" onClick={handleOpen}>
-        react-spring
-      </button> */}
       <Modal
         aria-labelledby="spring-modal-title"
         aria-describedby="spring-modal-description"
